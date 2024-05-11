0ec2ca49903e53f9ce7b4597680535021829ae15
alexmaxu <12alexmaxu@gmail.com>
2024-05-11 08:35:00 +0200

    Initial Commit

---
+++ testNetwork/testNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  testNetworkApp.swift
+//  testNetwork
+//
+//  Created by Alex  on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct testNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
